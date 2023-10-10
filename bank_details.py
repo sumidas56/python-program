2f9ad2daab80bdbe45954037fdfd25e522703341
sumidas <sumisivadasan91@gmail.com>
2023-10-10 08:53:19 +0530

    message

--- bank_details.py
+++ bank_details.py
@@ -1,17 +1,22 @@
 account={}
 account_list=[]
-
+acc_num=1000
 def create_account():
-        name=input("USER NAME")
-        address=input("ENTER THE ADDRESS")
-        age=input("USER AGE")
-        contact=input("CONTACT NUMBER")
+    global acc_num
+    name=input("USER NAME")
+    address=input("ENTER THE ADDRESS")
+    age=input("USER AGE")
+    contact=input("CONTACT NUMBER")
+    acc_bal=0
 
-        account['USERNAME']=name
-        account['ADDRESS']=address
-        account['AGE']=age
-        account['CONTACTNO']=contact
-        account_list.append(account.copy())
+    account['USERNAME']=name
+    account['ACCOUNT NO']=acc_num
+    account['ADDRESS']=address
+    account['AGE']=age
+    account['acc_bal']=acc_bal
+    account['CONTACTNO']=contact
+    account_list.append(account.copy())
+    acc_num+=1
 
 def account_details():
     print("accounts is as follows")
@@ -20,28 +25,46 @@ def account_details():
                 print(i, ':', j)
 
 def account_bal():
-    amount=int(input("enter your amount:"))
+    b=int(input("ENTER YOUR accnum"))
+    amount=int(input("ENTER YOUR AMOUNT"))
     account1=account_list.copy()
+    acc_number=[]
     for i in account1:
-        acc_bal=0
-        i.update({"acc_bal":acc_bal+amount})
+        acc_number.append(i['ACCOUNT NO'])
+    acc_number.append(i['ACCOUNT NO'])
+        
+    if  b in acc_number:
+        
+        for i in account1:
+            if i['ACCOUNT NO']==b:
+                acc_bal=i['acc_bal']           
+                i.update({"acc_bal":acc_bal+amount})
+
+
         for account in account1:
             for y,x in account.items():
                 print(y,':',x)
 
 def account_withdraw():
-    wid_amount=int(input("enter amount of withdraw"))
+    b=int(input("ENTER YOUR accnum"))
+    
     account1=account_list.copy()
+    acc_number=[]
     for i in account1:
-        acc_bal=i["acc_bal"]
-        if acc_bal>0:
-            if acc_bal<wid_amount:
-                print("insufficent balance")
-                break
-            else:
-                i.update({"acc_bal":acc_bal-wid_amount})
+       acc_number.append(i['ACCOUNT NO'])
+    if  b in acc_number:
+     wid_amount=int(input("enter amount of withdraw"))
+    for i in account1:
+        if i['ACCOUNT NO']==b:
+            acc_bal=i['acc_bal'] 
+            if acc_bal>0:
+               if acc_bal<wid_amount:
+                  print("insufficent balance")
+                  break
+               else:
+                  i.update({'acc_bal':acc_bal-wid_amount})
         else:
-            print("insufficent balance")
+            print("acc num not found")
             break
         for account in account1:
             for y,x in account.items():
